7f59c24a284695f36bc7205612714c806019d19f
prasanth-ikm <prashanthrs@ikm.gov.in>
2025-05-26 16:26:57 +0530

    product contoller data fix

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const ProductTable  = require("../models/product");
+const ProductTable = require("../models/product");
 const { responseHandler } = require("./response");
 
 exports.addProduct = async (req, res) => {
@@ -19,7 +19,7 @@ exports.addProduct = async (req, res) => {
 exports.updateProduct = async (req, res) => {
     try {
         let data = await ProductTable.updateMany({ _id: req.params.id }, {
-            $set: {                ...req.body            }
+            $set: { ...req.body }
         })
         responseHandler.success(res, data, "Product Updated Successfully", 200)
     } catch (err) {
@@ -32,7 +32,7 @@ exports.updateProduct = async (req, res) => {
 exports.listAllProduct = async (req, res) => {
     try {
         let data = await ProductTable.find().sort({ "created": -1 });
-        responseHandler.success(res, data, 'Fetched success', 200)
+        responseHandler.success(res, { content: data, count: data?.length }, 'Fetched success', 200)
     } catch (err) {
         if (err) {
             responseHandler.error(res, err.message, 500)
@@ -44,7 +44,7 @@ exports.listByCategory = async (req, res) => {
     try {
         const { mainCategory } = req.params;
         let data = await ProductTable.find({ mainCategory: mainCategory }).sort({ "created": -1 });
-        responseHandler.success(res, data, 'Fetched success', 200)
+        responseHandler.success(res, { content: data, count: data?.length }, 'Fetched success', 200)
     } catch (err) {
         if (err) {
             responseHandler.error(res, err.message, 500)
--- controllers/response.js
+++ controllers/response.js
@@ -1,62 +1,62 @@
 const createResponse = (statusCode, success, message, data = null, meta = null) => ({
-    statusCode,
-    success,
-    message,
-    ...(data && { data }),
-    ...(meta && { meta })
-  });
-  
-  const responseHandler = {
-    success: (res, data, message = 'Success', statusCode = 200, meta) => {
-      return res.status(statusCode).json(
-        createResponse(statusCode,true, message, data, meta)
-      );
-    },
-  
-    error: (res, message = 'Internal Server Error', statusCode = 500, error = null) => {
-      const response = createResponse(statusCode,false, message);
-      
-      if (error && process.env.NODE_ENV === 'development') {
-        response.error = error.message;
-        response.stack = error.stack;
-      }
-  
-      return res.status(statusCode).json(response);
-    },
-  
-    notFound: (res, message = 'Resource Not Found') => {
-      return responseHandler.error(res, message, 404);
-    },
-  
-    badRequest: (res, message = 'Bad Request', errors = null) => {
-      const response = createResponse(statusCode,false, message);
-      if (errors) response.errors = errors;
-      return res.status(400).json(response);
-    },
-  
-    unauthorized: (res, message = 'Unauthorized') => {
-      return responseHandler.error(res, message, 401);
-    },
-  
-    forbidden: (res, message = 'Forbidden') => {
-      return responseHandler.error(res, message, 403);
-    },
-  
-    validationError: (res, message = 'Validation Error', errors) => {
-      const response = createResponse(statusCode,false, message);
-      response.errors = errors;
-      return res.status(422).json(response);
-    },
-  
-    paginated: (res, data, pagination, message = 'Paginated results', statusCode = 200) => {
-        return res.status(statusCode).json({
-            statusCode,
-        success: true,
-        message,
-        data,
-        pagination
-      });
+  statusCode,
+  success,
+  message,
+  ...(data && { data }),
+  ...(meta && { meta })
+});
+
+const responseHandler = {
+  success: (res, data, message = 'Success', statusCode = 200, meta = '') => {
+    return res.status(statusCode).json(
+      createResponse(statusCode, true, message, data, meta)
+    );
+  },
+
+  error: (res, message = 'Internal Server Error', statusCode = 500, error = null) => {
+    const response = createResponse(statusCode, false, message);
+
+    if (error && process.env.NODE_ENV === 'development') {
+      response.error = error.message;
+      response.stack = error.stack;
     }
-  };
+
+    return res.status(statusCode).json(response);
+  },
+
+  notFound: (res, message = 'Resource Not Found') => {
+    return responseHandler.error(res, message, 404);
+  },
+
+  badRequest: (res, message = 'Bad Request', errors = null) => {
+    const response = createResponse(statusCode, false, message);
+    if (errors) response.errors = errors;
+    return res.status(400).json(response);
+  },
+
+  unauthorized: (res, message = 'Unauthorized') => {
+    return responseHandler.error(res, message, 401);
+  },
+
+  forbidden: (res, message = 'Forbidden') => {
+    return responseHandler.error(res, message, 403);
+  },
+
+  validationError: (res, message = 'Validation Error', errors = '') => {
+    const response = createResponse(statusCode, false, message);
+    response.errors = errors;
+    return res.status(422).json(response);
+  },
+
+  paginated: (res, data, pagination, message = 'Paginated results', statusCode = 200) => {
+    return res.status(statusCode).json({
+      statusCode,
+      success: true,
+      message,
+      data,
+      pagination
+    });
+  }
+};
 
 module.exports = { responseHandler }
